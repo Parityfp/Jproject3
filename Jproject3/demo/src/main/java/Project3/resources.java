c909be6f1c67c6658a3b770235b8fcaea2e5ecb1
parityfp <peso7b@gmail.com>
2023-11-28 01:59:07 +0700

    forgot

--- Jproject3/demo/src/main/java/Project3/resources.java
+++ Jproject3/demo/src/main/java/Project3/resources.java
@@ -49,3 +49,31 @@ interface MyConstants
 
 
 //for handling audio
+class MySoundEffect
+{
+    private Clip         clip;
+    private FloatControl gainControl;         
+
+    public MySoundEffect(String filename)
+    {
+	try
+	{
+            java.io.File file = new java.io.File(filename);
+            AudioInputStream audioStream = AudioSystem.getAudioInputStream(file);
+            clip = AudioSystem.getClip();
+            clip.open(audioStream);            
+            gainControl = (FloatControl)clip.getControl(FloatControl.Type.MASTER_GAIN);
+	}
+	catch (Exception e) { e.printStackTrace(); }
+    }
+    public void playOnce()             { clip.setMicrosecondPosition(0); clip.start(); }
+    public void playLoop()             { clip.loop(Clip.LOOP_CONTINUOUSLY); }
+    public void stop()                 { clip.stop(); }
+    public void setVolume(float gain)
+    {
+        if (gain < 0.0f)  gain = 0.0f;
+        if (gain > 1.0f)  gain = 1.0f;
+        float dB = (float)(Math.log(gain) / Math.log(10.0) * 20.0);
+        gainControl.setValue(dB);
+    }
+}
